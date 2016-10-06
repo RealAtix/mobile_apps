9f10343fb1273362cf4894011a91fa7b48943ef6
Razzy <razvan.nicolescu@student.ucll.be>
2016-10-06 17:40:06 +0200

    Added interaction

--- Chronometer/app/src/main/java/io/raztech/chronometer/MainActivity.java
+++ Chronometer/app/src/main/java/io/raztech/chronometer/MainActivity.java
@@ -2,12 +2,54 @@ package io.raztech.chronometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+
+    TextView timeView;
+    Button startStopButton;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timeView = (TextView) findViewById(R.id.timeView);
+        startStopButton = (Button) findViewById(R.id.startStopButton);
+        resetButton = (Button) findViewById(R.id.resetButton);
+
+        startStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increment();
+                showCount();
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+                showCount();
+            }
+        });
+    }
+
+    private void showCount() {
+        timeView.setText(String.valueOf(count));
+        Log.d("showCount", timeView.getText().toString());
+    }
+
+    private void increment() {
+        count++;
+    }
+
+    private void reset() {
+        count = 0;
     }
 }
