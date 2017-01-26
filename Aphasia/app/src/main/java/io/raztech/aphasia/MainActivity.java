d30c5bdc851f25cff892ae1cd9127280fbf49c37
Razzy <razvan.nicolescu@student.ucll.be>
2017-01-26 18:58:04 +0100

    Finished test A resultfile output + added audio permission check

--- Aphasia/app/src/main/java/io/raztech/aphasia/MainActivity.java
+++ Aphasia/app/src/main/java/io/raztech/aphasia/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private String name;
 
     private static final int REQUEST_STORAGE_PERMISSION = 114;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         boolean hasPermission = (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
-                (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED);
         if (!hasPermission) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
                     REQUEST_STORAGE_PERMISSION);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_RECORD_AUDIO_PERMISSION);
         }
 
         getName();
@@ -77,6 +83,14 @@ public class MainActivity extends AppCompatActivity {
         startTest1Activity();
     }
 
+    protected void onEndClicked(View v) {
+        // restart
+        Intent i = getBaseContext().getPackageManager()
+                .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+    }
+
     protected void nameInputError() {
         Toast.makeText(this, "Need input", Toast.LENGTH_SHORT).show();
     }
@@ -96,10 +110,32 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
                     Log.v("Permission","Permission is granted");
-                } else
-                {
+                } else {
                     Log.v("Permission","Permission is denied");
                     Toast.makeText(this, "This app needs external card READ and WRITE permission to work.", Toast.LENGTH_LONG).show();
+
+                    Thread closeActivity = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(3000);
+
+                                finish();
+                                moveTaskToBack(true);
+                            } catch (Exception e) {
+                                e.getLocalizedMessage();
+                            }
+                        }
+                    });
+                }
+            }
+            case REQUEST_RECORD_AUDIO_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Log.v("Permission","Permission is granted");
+                } else {
+                    Log.v("Permission","Permission is denied");
+                    Toast.makeText(this, "This app needs audio recording permission to work.", Toast.LENGTH_LONG).show();
 
                     Thread closeActivity = new Thread(new Runnable() {
                         @Override
--- Aphasia/app/src/main/java/io/raztech/aphasia/Test_1.java
+++ Aphasia/app/src/main/java/io/raztech/aphasia/Test_1.java
@@ -1,7 +1,9 @@
 package io.raztech.aphasia;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Environment;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.Voice;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +16,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
@@ -31,7 +36,7 @@ import java.util.Map;
 import io.raztech.aphasia.services.AsyncResponse;
 import io.raztech.aphasia.services.Test1Loader;
 
-public class Test_1 extends AppCompatActivity implements AsyncResponse {
+public class Test_1 extends AppCompatActivity implements AsyncResponse, TextToSpeech.OnInitListener {
 
     private Map<Integer, List<File>> images;
     private Map<Integer, JSONObject> info;
@@ -39,6 +44,8 @@ public class Test_1 extends AppCompatActivity implements AsyncResponse {
     private TextView wordText;
     private int counter;
     private TextToSpeech tts;
+    private int MY_DATA_CHECK_CODE = 0;
+    private JSONArray jsonAnswerArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,18 +53,25 @@ public class Test_1 extends AppCompatActivity implements AsyncResponse {
         setContentView(R.layout.activity_test_1);
 
         counter = 0;
+        jsonAnswerArray = new JSONArray();
 
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
         Log.i("name", name);
 
+        //check for TTS data
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+
+        //data
         File rootDataDir = new File(this.getExternalFilesDir(null).getAbsolutePath() + "/test_1");
         Log.d("Path", rootDataDir.toString());
 
         File testDir = new File(rootDataDir.getAbsolutePath() + "/0");
         if(!testDir.exists()) {
             Toast.makeText(this, "Test data is missing from external storage", Toast.LENGTH_LONG).show();
-            return;
+            //return;
         } else {
             File[] dirFiles = testDir.listFiles();
             Log.d("files", dirFiles.toString());
@@ -69,17 +83,34 @@ public class Test_1 extends AppCompatActivity implements AsyncResponse {
 
         wordText = (TextView) findViewById(R.id.txtWord);
 
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
-                    tts.setLanguage(Locale.UK);
-                    //Voice k = new Voice("l", Locale.UK, Voice.QUALITY_VERY_HIGH, Voice.LATENCY_VERY_LOW, true, null);
-                    //tts.setVoice(k);
-                }
-            }
-        });
+    }
 
+    @Override
+    public void onInit(int status) {
+        //check for successful instantiation
+        if (status == TextToSpeech.SUCCESS) {
+            if(tts.isLanguageAvailable(Locale.US)==TextToSpeech.LANG_AVAILABLE)
+                tts.setLanguage(Locale.US);
+        }
+        else if (status == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                //the user has the necessary data - create the TTS
+                tts = new TextToSpeech(this, this);
+            }
+            else {
+                //no data, install it now
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
     }
 
     @Override
@@ -92,10 +123,16 @@ public class Test_1 extends AppCompatActivity implements AsyncResponse {
     }
 
     private void showNextQuestion() {
-        Log.d("info", info.get(counter).toString());
+
+        if(counter >= info.size()) {
+            testDone();
+            return;
+        }
+
+        //Log.d("showNextQuestion", info.get(counter).toString());
 
         try {
-            Log.d("word", info.get(counter).getString("word"));
+            Log.d("wordNextQuestion", info.get(counter).getString("word"));
             wordText.setText(info.get(counter).getString("word"));
         } catch (JSONException e) {
             e.printStackTrace();
@@ -119,25 +156,99 @@ public class Test_1 extends AppCompatActivity implements AsyncResponse {
         counter++;
     }
 
+    private void testDone() {
+
+        File resultDir = new File(this.getExternalFilesDir(null).getAbsolutePath() + "/results/" + name.replaceAll("\\s+","_"));
+        File resultFile = new File(this.getExternalFilesDir(null).getAbsolutePath() + "/results/" + name.replaceAll("\\s+","_") +
+                "/" + name.replaceAll("\\s+","_") + "_test1.json");
+        JSONObject jsonObject = new JSONObject();
+
+        boolean success = true;
+        if (!resultDir.exists()) {
+            success = resultDir.mkdirs();
+        }
+        if (success) {
+            try {
+                jsonObject.put("name", name);
+                jsonObject.put("test", "1");
+                jsonObject.put("answers", jsonAnswerArray);
+                Log.d("jsonobj", jsonObject.toString());
+
+                FileWriter file = new FileWriter(resultFile);
+                file.write(jsonObject.toString(4));
+                file.flush();
+                file.close();
+            } catch (JSONException | IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        tts.shutdown();
+        finish();
+    }
+
     protected void onPlayClicked(View v) {
-        //tts.speak("hello", TextToSpeech.QUEUE_FLUSH, null, null);
-        tts.speak(wordText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+        for (Voice tmpVoice : tts.getVoices()) {
+            //Log.d("Voice: ", tmpVoice.toString());
+            if (tmpVoice.getName().equals("en-gb-x-fis#female_1-local")) {
+                tts.setVoice(tmpVoice);
+            }
+        }
+        tts.speak(wordText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null, null);
+        //tts.speak(wordText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
     }
 
     protected void onImg1Clicked(View v) {
-        showNextQuestion();
+        try {
+            JSONObject answer = new JSONObject();
+            answer.put("number", info.get(counter-1).getString("number"));
+            answer.put("answer", "1");
+            jsonAnswerArray.put(answer);
+
+            showNextQuestion();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void onImg2Clicked(View v) {
-        showNextQuestion();
+        try {
+            JSONObject answer = new JSONObject();
+            answer.put("number", info.get(counter-1).getString("number"));
+            answer.put("answer", "2");
+            jsonAnswerArray.put(answer);
+
+            showNextQuestion();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void onImg3Clicked(View v) {
-        showNextQuestion();
+        try {
+            JSONObject answer = new JSONObject();
+            answer.put("number", info.get(counter-1).getString("number"));
+            answer.put("answer", "3");
+            jsonAnswerArray.put(answer);
+
+            showNextQuestion();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void onImg4Clicked(View v) {
-        showNextQuestion();
+        try {
+            JSONObject answer = new JSONObject();
+            answer.put("number", info.get(counter-1).getString("number"));
+            answer.put("answer", "4");
+            jsonAnswerArray.put(answer);
+
+            showNextQuestion();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 }
